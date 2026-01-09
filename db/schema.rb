40c88c016ab74cee8ba7cc011335817ed2ac9a83
Anh Nhật <nhathuynh04work@gmail.com>
2026-01-09 21:30:24 +0700

    create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_07_064152) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_09_142953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_07_064152) do
     t.datetime "created_at", null: false
     t.integer "status", default: 0
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 end
