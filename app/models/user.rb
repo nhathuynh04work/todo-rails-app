89eedf3eb3aa2225c8be3c264f530f9fd86510b0
Anh Nhật <nhathuynh04work@gmail.com>
2026-01-09 22:45:57 +0700

    update the User model by adding validations, callbacks and add index to email column of users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  normalizes :first_name, with: ->(first_name) { first_name.strip.titleize }
+  normalizes :last_name, with: ->(last_name) { last_name.strip.titleize }
+  normalizes :email, with: ->(email) { email.strip.downcase }
+
+  has_secure_password
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :first_name, :last_name,
+            presence: true
+
+  validates :password,
+            allow_nil: true,
+            presence: true,
+            length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_09_142953) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_09_153934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,5 +28,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_09_142953) do
     t.string "last_name"
     t.string "password_digest"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
