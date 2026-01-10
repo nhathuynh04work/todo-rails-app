f9a41b812e1cbd3b4eb19558517faccb89bd0dc8
Anh Nhật <nhathuynh04work@gmail.com>
2026-01-10 16:18:41 +0700

    implement login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Login successfully!"
+    else
+      flash[:alert] = "Invalid email/password"
+      render "new", status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post "/signup", to: "registrations#create"
 
   get "/login", to: "sessions#new", as: :login
+  post "/login", to: "sessions#create"
 
+  resources :dashboard, only: [ :show ]
   root "static_pages#home"
 end
